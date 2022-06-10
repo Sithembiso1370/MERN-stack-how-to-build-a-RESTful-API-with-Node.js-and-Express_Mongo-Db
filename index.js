80c381a0a8b2f096cd9e10f13bfd2484ac6a8d51
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-06-10 19:23:18 +0200

    complete and connecting to db need to test crud and how to connect to frontend

---
+++ index.js
@@ -0,0 +1,8 @@
+//import the app object
+import { app } from './app.js'
+//get the port from .env
+import { PORT } from './utils/config.js'
+//run the server on the app object with the defined port
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
